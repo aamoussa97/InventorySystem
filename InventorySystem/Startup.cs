aa65b3b47290631e7eb8f199b5d20f8b9bc9abd2
Oliver <oliverskoeppen@gmail.com>
2019-04-05 08:55:25 +0200

    Added swaggerUI and deleted unused controller

--- InventorySystem/Startup.cs
+++ InventorySystem/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace InventorySystem
 {
@@ -28,7 +29,11 @@ namespace InventorySystem
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-          
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info {Title="Inventory System API", Version="V1"});
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +48,14 @@ namespace InventorySystem
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Inventory System in dev");
+                c.RoutePrefix = string.Empty;
+            });
 
             //app.UseHttpsRedirection();
             app.UseMvc();
