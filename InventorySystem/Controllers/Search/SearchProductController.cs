ba329587df75e732b259e4a860a6f940f89fc292
AM <lgv2800@gmail.com>
2019-04-14 12:31:23 +0200

    Readded productSearchController, as OData search in Products is broken atm.

---
+++ InventorySystem/Controllers/Search/SearchProductController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InventorySystem.DataLayerClasses;
+using InventorySystem.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace InventorySystem.Controllers
+{
+    [Route("api/search/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        readonly IConfiguration _configuration;
+
+        public ProductController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // GET: api/product/name
+        [HttpGet("{ProductName}")]
+        public IEnumerable<ProductsGET> Get(string ProductName)
+        {
+            return new ProductsDataLayer(_configuration).GetProductByName(ProductName);
+        }
+
+    }
+}
