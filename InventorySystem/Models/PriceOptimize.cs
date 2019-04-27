2590b3da6b748b06cb9ac7df66362ab7c38cbf77
sebdtu <32480015+sebdtu@users.noreply.github.com>
2019-04-27 12:44:41 +0200

    PrizeOptimize

---
+++ InventorySystem/Models/PriceOptimize.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventorySystem.Models
+{
+    public class PriceOptimize
+    {
+        ProductsDataLayer productsDataLayer = new ProductsDataLayer();
+        public int OptimizePrice(int id)
+        {
+            Product product = productsDataLayer.GetProduct(id);
+            double vCost = VcostDataLayer.GetProductVariableCost(product.ProductVariableCostID);
+            double startFactor = 5000; //TODO Add datalayer class to get the products startfactor
+            double growthFactor = 0.5; //TODO Add datalayer class to get the products growthfactor
+            double marginalRevenue = growthFactor * 2;
+            double optimalAmount = startFactor / growthFactor - marginalRevenue / growthFactor;
+            double optimalPrice = startFactor - growthFactor * optimalAmount;
+            return optimalPrice;
+        }
+    }
+}
